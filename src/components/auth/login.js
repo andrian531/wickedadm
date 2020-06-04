4a3435d5e1b0c9fec2aec6c7ceeaba10c854f07c
andrianto <andrianto531@gmail.com>
2020-06-04 14:15:10 +0700

    back
    back

---
+++ src/components/auth/login.js
@@ -0,0 +1,42 @@
+import React, { Component, Fragment } from 'react'
+import LoginTabset from './loginTabset';
+import { ArrowLeft, Sliders } from 'react-feather';
+import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
+import stats from '../../assets/images/dashboard/stats.png';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+export class Login extends Component {
+    render() {
+        var settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            arrows: false
+        };
+        return (
+            <Fragment>
+                <div className="page-wrapper">
+                    <div className="authentication-box">
+                        <div className="container">
+                            <div className="row">
+                                
+                                <div className="col-md-12 p-0 card-right">
+                                    <div className="card tab2-card">
+                                        <div className="card-body">
+                                            <LoginTabset />
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                    </div>
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+export default Login
---
+++ src/components/auth/loginTabset.js
@@ -0,0 +1,71 @@
+import React, { Component, Fragment } from 'react';
+import { Tabs, TabList, TabPanel, Tab } from 'react-tabs';
+import { User, Unlock } from 'react-feather';
+import { withRouter } from 'react-router-dom';
+
+export class LoginTabset extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeShow: true,
+            startDate: new Date()
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    clickActive = (event) => {
+        document.querySelector(".nav-link").classList.remove('show');
+        event.target.classList.add('show');
+    }
+    handleChange(date) {
+        this.setState({
+            startDate: date
+        });
+    }
+
+    routeChange = () => {
+        this.props.history.push(`${process.env.PUBLIC_URL}/dashboard`);
+      }
+    render() {
+        return (
+            <div>
+                <Fragment>
+                    <Tabs>
+                        <TabList className="nav nav-tabs tab-coupon" >
+                            <Tab className="nav-link" onClick={(e) => this.clickActive(e)}><User />Login</Tab>
+                          
+                        </TabList>
+
+                        <TabPanel>
+                            <form className="form-horizontal auth-form">
+                                <div className="form-group">
+                                    <input required="" name="login[username]" type="email" className="form-control" placeholder="Username" id="exampleInputEmail1" />
+                                </div>
+                                <div className="form-group">
+                                    <input required="" name="login[password]" type="password" className="form-control" placeholder="Password" />
+                                </div>
+                                <div className="form-terms">
+                                    <div className="custom-control custom-checkbox mr-sm-2">
+                                        <input type="checkbox" className="custom-control-input" id="customControlAutosizing" />
+                                        <label className="d-block">
+                                                    <input className="checkbox_animated" id="chk-ani2" type="checkbox" />
+                                                        Reminder Me <span className="pull-right"></span>
+                                                </label>
+                                    </div>
+                                </div>
+                                <div className="form-button">
+                                    <button className="btn btn-primary" type="submit"  onClick={() => this.routeChange()}>Login</button>
+                                </div>
+                               
+                            </form>
+                        </TabPanel>
+                       
+                    </Tabs>
+                </Fragment>
+            </div>
+        )
+    }
+}
+
+export default withRouter(LoginTabset)
+
